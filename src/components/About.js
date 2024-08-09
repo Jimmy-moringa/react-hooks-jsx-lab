83aa5ccffea417261c7f68a9389fd28f580de8c7
jimmy-moringa <jimmy.musyoki@student.moringaschool.com>
2024-08-09 08:46:16 +0300

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <img src={image} alt="I made this" />
+    <p>Welcome to our website!. You can call me Jimmy</p>
+  </div>;
 }
 
 export default About;
